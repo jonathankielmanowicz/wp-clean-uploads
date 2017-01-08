5dd99e594465cec7a99cc6e852b70b7ba5c88c23
Jonathan Kielmanowicz <jonathan.kielmanowicz@gmail.com>
2017-01-08 18:15:09 -0500

    changed from post type to admin menu

--- clean-uploads.php
+++ clean-uploads.php
@@ -7,52 +7,19 @@
 * Version: 0.0.1
 */
 
-//Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+function cu_clean_media_html()
+{
+    // check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+    require plugin_dir_path(__FILE__) . 'cu-menu.php';
 }
 
-function cu_register_clean_media() {
+function cu_clean_media() {
 
-  if( current_user_can('manage_options') ) {
-
-    $args = array(
-      'public'              => true,
-      'label'               => 'Clean Media',
-      'publicly_queryable'  => true,
-      'exclude_from_search' => true,
-      'show_in_nav_menus'   => false,
-      'show_ui'             => true,
-      'show_in_menu'        => true,
-      'show_in_admin_bar'   => false,
-      'menu_position'       => 10,
-      'menu_icon'           => 'dashicons-trash',
-      'can_export'          => true,
-      'delete_with_user'    => false,
-      'hierarchical'        => false,
-      'has_archive'         => true,
-      'query_var'           => true,
-      'capability_type'     => 'post',
-      'map_meta_cap'        => true,
-      // 'capabilities' => array(),
-      'rewrite'             => array(
-        'slug' => $slug,
-        'with_front' => true,
-        'pages' => true,
-        'feeds' => true,
-        ),
-      'supports'            => array(
-        'title',
-        'editor',
-        'author',
-        'custom-fields'
-        )
-      );
-
-    register_post_type( 'clean_media', $args);
-
-  }
+  add_menu_page( 'Clean Media', 'Clean Media', 'manage_options', 'clean-media', 'cu_clean_media_html', 'dashicons-trash', 10 );
 
 }
 
-add_action( 'init', 'cu_register_clean_media' );
+add_action( 'admin_menu', 'cu_clean_media' );
